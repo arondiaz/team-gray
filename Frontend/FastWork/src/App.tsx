d6b483daf34b6cca422fcea64eadf0de250c1d42
hernan-go <gobulinhernan@gmail.com>
2023-07-09 19:29:13 -0300

    create navbar component with styles

--- Frontend/FastWork/src/App.tsx
+++ Frontend/FastWork/src/App.tsx
@@ -1,11 +1,13 @@
+import Header from './components/NavigationMenu/Header';
 import { RouteSwitch } from './routes/RouteSwitch/RouteSwitch';
 
 function App() {
-  return (
-    <>
-      <RouteSwitch />
-    </>
-  );
+	return (
+		<>
+			<Header />
+			<RouteSwitch />
+		</>
+	);
 }
 
 export default App;
