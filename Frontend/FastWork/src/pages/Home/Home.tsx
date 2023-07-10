9771f1536d27227c17e15eccce54c10c1efd1ec9
hernan-go <gobulinhernan@gmail.com>
2023-07-10 13:21:29 -0300

    start designing home page (WIP)

--- Frontend/FastWork/src/pages/Home/Home.tsx
+++ Frontend/FastWork/src/pages/Home/Home.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import styles from './Home.module.scss'; // Importa los estilos del archivo .module.scss
+import styles from './Home.module.scss';
+import personImage from '../../shared/person_image.png'; // Ruta a la imagen de la persona
 
-export const Home = () => {
+export const Home: React.FC = () => {
 	return (
 		<div className={styles.homeContainer}>
-			<h1 className={styles.homeTitle}>Home</h1>
+			<div className={styles.logoContainer}>
+				<img src={personImage} alt='Persona' className={styles.personImage} />
+				<img
+					src='../../../public/isotipo_logo.svg'
+					alt='Logo'
+					className={styles.logo}
+				/>
+			</div>
+			<div className={styles.textContainer}>
+				<h1 className={styles.title}>Conectando habilidades.</h1>
+				<h2 className={styles.subtitle}>Construyendo oportunidades.</h2>
+			</div>
 		</div>
 	);
 };
