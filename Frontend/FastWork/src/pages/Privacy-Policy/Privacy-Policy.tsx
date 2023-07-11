d3b17cc0f21e7903d641907244d66009a90cee24
NicoLoreto <nicoloreto.dev@gmail.com>
2023-07-10 21:17:37 -0300

    new branch feature/privacy-policy-page

--- Frontend/FastWork/src/pages/Privacy-Policy/Privacy-Policy.tsx
+++ Frontend/FastWork/src/pages/Privacy-Policy/Privacy-Policy.tsx
@@ -1,9 +1,9 @@
 import styles from './Privacy-Policy.module.scss';
 
 export const PrivacyPolicy = () => {
-	return (
-		<div className={styles.pageContainer}>
-			<h1 className={styles.pageTitle}>Política de Privacidad</h1>
-		</div>
-	);
+  return (
+    <div className={styles.pageContainer}>
+      <h1 className={styles.pageTitle}>Política de Privacidad</h1>
+    </div>
+  );
 };
