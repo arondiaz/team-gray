53f632c286ae2d21d7d2cedd61a67a092f0e4120
hernan-go <gobulinhernan@gmail.com>
2023-07-12 06:46:14 -0300

    refactored code and added Client/Professional user buttons

--- Frontend/FastWork/src/pages/Home/Home.tsx
+++ Frontend/FastWork/src/pages/Home/Home.tsx
@@ -1,22 +1,37 @@
-import React from 'react';
 import styles from './Home.module.scss';
-import personImage from '../../shared/person_image.png'; // Ruta a la imagen de la persona
+import personImage from '../../shared/person_image.png';
+import ProfessionalUserButton from './Users-Buttons/ProfessionalUserButton';
+import ClientUserButton from './Users-Buttons/ClientUserButton';
 
 export const Home: React.FC = () => {
 	return (
 		<div className={styles.homeContainer}>
-			<div className={styles.logoContainer}>
-				<img src={personImage} alt='Persona' className={styles.personImage} />
+			{/* Person image / logo container */}
+			<div className={styles.isotipoLogoContainer}>
 				<img
-					src='../../../public/isotipo_logo.svg'
+					src={personImage}
+					alt='Person using a smartphone'
+					className={styles.personImage}
+				/>
+
+				<img
+					src='/isotype_logo.svg'
 					alt='Logo'
-					className={styles.logo}
+					className={styles.isotipoLogo}
 				/>
 			</div>
-			<div className={styles.textContainer}>
-				<h1 className={styles.title}>Conectando habilidades.</h1>
-				<h2 className={styles.subtitle}>Construyendo oportunidades.</h2>
+
+			{/* Slogan text */}
+			<div className={styles.textSloganContainer}>
+				<p className={styles.slogan}>Conectando habilidades.</p>
+				<p className={styles.slogan}>Construyendo oportunidades.</p>
+
+				{/* Access buttons */}
+				<ProfessionalUserButton />
+				<ClientUserButton />
 			</div>
 		</div>
 	);
 };
+
+export default Home;
