44d2b71afe44c6c3f0b6e213ddf2a2c4841aed91
hernan-go <gobulinhernan@gmail.com>
2023-07-13 04:08:47 -0300

    fix duplicate file

--- Frontend/FastWork/src/components/HeaderMenu/Header.tsx
+++ Frontend/FastWork/src/components/HeaderMenu/Header.tsx
@@ -2,17 +2,25 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { BiMenuAltRight } from 'react-icons/bi';
 import { AiOutlineClose } from 'react-icons/ai';
-
 import classes from './Header.module.scss';
 
+{
+	/* State to control the menu open/close */
+}
 const Header = () => {
 	const [menuOpen, setMenuOpen] = useState(false);
 
+	{
+		/* State to store window size*/
+	}
 	const [size, setSize] = useState({
 		width: 0,
 		height: 0,
 	});
 
+	{
+		/* Handle window resize event */
+	}
 	useEffect(() => {
 		const handleResize = () => {
 			setSize({
@@ -20,97 +28,99 @@ const Header = () => {
 				height: window.innerHeight,
 			});
 		};
+
 		window.addEventListener('resize', handleResize);
 
+		{
+			/* Cleanup event listener on component unmount */
+		}
 		return () => window.removeEventListener('resize', handleResize);
 	}, []);
 
+	{
+		/* Close menu if window width is greater than 768px */
+	}
 	useEffect(() => {
 		if (size.width > 768 && menuOpen) {
 			setMenuOpen(false);
 		}
 	}, [size.width, menuOpen]);
 
+	{
+		/* Toggle menu open/close state */
+	}
 	const menuToggleHandler = () => {
-		setMenuOpen((p) => !p);
+		setMenuOpen((prevMenuOpen) => !prevMenuOpen);
 	};
 
-	const [activeLink, setActiveLink] = useState('/');
-
-	const handleLinkClick = (
-		event: React.MouseEvent<HTMLAnchorElement>,
-		link: string
-	) => {
-		event.preventDefault();
-		setActiveLink(link);
-	};
+	{
+		/* Active link state (currently set to '/' */
+	}
+	const [activeLink] = useState('/');
 
 	return (
 		<header className={classes.header}>
 			<div className={classes.header__content}>
 				<div className={classes.header__content__logo}>
-					{/* Navigation logo & items */}
-					<img src='/logo_fastwork_purple.svg'></img>
+					<img src='/logo_fastwork_purple.svg' alt='Logo' />
 				</div>
-
-				{/* Navigation block */}
 				<nav
 					className={`${classes.header__content__nav} ${
 						menuOpen ? classes.isMenu : ''
 					}`}>
 					<ul>
-						{/* Navigation item: Home */}
+						{/* Home link */}
+
 						<li>
 							<Link
 								to='/'
-								className={
-									activeLink === '/'
-										? `${classes.active} ${classes.activeBar}`
-										: ''
-								}>
+								className={`${activeLink === '/' ? classes.active : ''} ${
+									classes.activeBar
+								}`}>
 								Home
 							</Link>
 						</li>
-						{/* Navigation item: Oficios */}
+
+						{/* Trades link */}
+
 						<li>
 							<Link
-								to='trades'
-								className={
-									activeLink === '/trades'
-										? `${classes.active} ${classes.activeBar}`
-										: ''
-								}>
+								to='/trades'
+								className={`${activeLink === '/trades' ? classes.active : ''} ${
+									classes.activeBar
+								}`}>
 								Oficios
 							</Link>
 						</li>
-						{/* Navigation item: Sobre nosotros */}
+
+						{/* About link */}
+
 						<li>
 							<Link
 								to='/about'
-								className={
-									activeLink === '/nosotros'
-										? `${classes.active} ${classes.activeBar}`
-										: ''
-								}>
+								className={`${
+									activeLink === '/nosotros' ? classes.active : ''
+								} ${classes.activeBar}`}>
 								Sobre nosotros
 							</Link>
 						</li>
-						{/* Navigation item: Política */}
+
+						{/* Privacy Policy link */}
+
 						<li>
 							<Link
 								to='/privacy-policy'
-								className={
-									activeLink === '/politica'
-										? `${classes.active} ${classes.activeBar}`
-										: ''
-								}>
+								className={`${
+									activeLink === '/politica' ? classes.active : ''
+								} ${classes.activeBar}`}>
 								Política
 							</Link>
 						</li>
 					</ul>
 				</nav>
-
 				<div className={classes.header__content__toggle}>
+					{/* Menu toggle button */}
+
 					{!menuOpen ? (
 						<BiMenuAltRight onClick={menuToggleHandler} size={40} />
 					) : (
