17a8566b881915fc97f6f6e3a232d4c687a3ff0f
NicoLoreto <nicoloreto.dev@gmail.com>
2023-07-14 16:20:08 -0300

    IRoute and map in routes (Trello ID: bykxyeqz)(WIP)

--- src/website/src/routes/RouteSwitch/RouteSwitch.tsx
+++ src/website/src/routes/RouteSwitch/RouteSwitch.tsx
@@ -1,23 +1,17 @@
-import { Route, Routes, Navigate } from 'react-router-dom';
-import { About, Trades, Home, PrivacyPolicy, ErrorPage } from '../../pages';
-import { Footer } from '../../components/Footer/Footer';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Header from '../../components/HeaderMenu/Header';
-import {Login} from "../../auth/Login/Login"
+import { routes } from '../routes';
+import { Footer } from '../../components/Footer/Footer';
+import { Home } from '../../pages';
 
 export const RouteSwitch = () => {
   return (
     <>
       <Header />
       <Routes>
-        <Route path='/' element={<Navigate to={'home'} />}></Route>
-        <Route path='home' element={<Home />}></Route>
-        <Route path='trades' element={<Trades />}></Route>
-        <Route path='about' element={<About />}></Route>
-        <Route path='privacy-policy' element={<PrivacyPolicy />}></Route>
-        <Route path='404' element={<ErrorPage />}></Route>
-        <Route path='Login' element={<Login />}></Route>
-
-        <Route path='*' element={<Navigate to='/404' />}></Route>
+        {routes.map((route) => (
+          <Route path={route.url} element={route.element}></Route>
+        ))}
       </Routes>
 
       <Footer />
--- src/website/src/routes/routes.tsx
+++ src/website/src/routes/routes.tsx
@@ -1,3 +1,57 @@
-export const routes = () => {
-  return <div>routes</div>;
-};
+import { Login } from '../auth/Login/Login';
+import { Signup } from '../auth/Signup/Signup';
+import { About, ErrorPage, Home, PrivacyPolicy, Trades } from '../pages';
+
+export interface IRoute {
+  url: string;
+  label?: string;
+  icon?: string;
+  isPublic?: boolean;
+  showNavigation?: boolean;
+  element: JSX.Element;
+}
+
+export const routes = Object.freeze<IRoute[]>([
+  {
+    url: 'login',
+    label: 'Log In',
+    isPublic: true,
+    element: <Login />,
+  },
+  {
+    url: 'signup',
+    label: 'Signup',
+    showNavigation: true,
+    isPublic: true,
+    element: <Signup />,
+  },
+  {
+    url: 'home',
+    label: 'Home',
+    isPublic: true,
+    element: <Home />,
+  },
+  {
+    url: 'trades',
+    label: 'Trades',
+    isPublic: true,
+    element: <Trades />,
+  },
+  {
+    url: 'about',
+    label: 'About',
+    isPublic: true,
+    element: <About />,
+  },
+  {
+    url: 'privacy-policy',
+    label: 'Privacy Policy',
+    isPublic: true,
+    element: <PrivacyPolicy />,
+  },
+  {
+    url: '*',
+    isPublic: false,
+    element: <ErrorPage />,
+  },
+]);
