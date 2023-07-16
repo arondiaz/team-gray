ee6cd11fb0bf8452102e9fd3ac64065e00399158
hernan-go <gobulinhernan@gmail.com>
2023-07-16 20:39:10 -0300

    implement component background layout (id Trello: QBuKCCzy)

---
+++ src/website/src/shared/BgLayout.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import style from './BgLayout.module.scss';
+
+export const BgLayout: React.FC = () => {
+	return (
+		<div className={style.isotipoLogoContainer}>
+			<img
+				src='../isotype_bg_layout.svg'
+				alt='Logo'
+				className={style.isotipoLogo}
+			/>
+		</div>
+	);
+};
+
+export default BgLayout;
