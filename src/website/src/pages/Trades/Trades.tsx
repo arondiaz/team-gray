881bc0f3a98e0055e1abca1df7b5460acfecfcdf
hernan-go <gobulinhernan@gmail.com>
2023-07-17 03:49:03 -0300

    implemented three-column layout

--- src/website/src/pages/Trades/Trades.tsx
+++ src/website/src/pages/Trades/Trades.tsx
@@ -3,11 +3,11 @@ import BgLayout from '../../shared/BgLayout';
 
 export const Trades = () => {
 	return (
-		<>
-			<div className={styles.pageContainer}>
-				<h1 className={styles.pageTitle}>Oficios</h1>
-			</div>
+		<div className={styles.homepage}>
+			<div className={styles['left-column']}>izquierda</div>
+			<div className={styles['middle-column']}>medio</div>
+			<div className={styles['right-column']}>derecha</div>
 			<BgLayout />
-		</>
+		</div>
 	);
 };
