5aa59666d9cad10672b74a5e7baf202b01d6416c
NicoLoreto <nicoloreto.dev@gmail.com>
2023-07-18 19:30:57 -0300

    validation on input of login-user (Trello ID: MQseDAzj) (WIP)

--- src/website/src/auth/Login/Login.tsx
+++ src/website/src/auth/Login/Login.tsx
@@ -1,82 +1,108 @@
-import { useState} from "react";
-import { FaArrowAltCircleLeft } from "react-icons/fa";
+import { useState } from 'react';
+import { FaArrowAltCircleLeft } from 'react-icons/fa';
 
-import styles from "./Login.module.scss";
+import styles from './Login.module.scss';
 
 export const Login = () => {
-
   const [form, setForm] = useState({
-    email: "",
-    password: ""
+    email: '',
+    password: '',
   });
 
   const [errorMail, setErrorMail] = useState(false);
   const [errorPassword, setErrorPassword] = useState(false);
-  // const [isValid, setIsValid] = useState(true);
+  const [isValid, setIsValid] = useState(true);
+  const [message, setMessage] = useState(false);
 
- 
-  const handleSubmit = (event:any) => {
+  const handleSubmit = (event: any) => {
     event.preventDefault();
-   
 
-    setForm({
-      email: '',
-      password: ''
-    });
-
-   
+    if (isValid && !errorMail && !errorPassword) {
+      setForm({
+        email: '',
+        password: '',
+      });
+      setMessage(false);
+    } else {
+      setMessage(true);
+    }
   };
+  //
+  const handleInputEmail = (event: any) => {
+    event.preventDefault();
 
+    if (event.target.value.length === 0) {
+      setErrorMail(true);
+    }
+    if (event.target.value.length > 0) {
+      setErrorMail(false);
+    }
 
-  
+    if (validation(form.email)) {
+      setIsValid(true);
+    }
 
+    if (!validation(form.email)) {
+      setIsValid(false);
+    }
 
-
-   // Validate Mail
-  //  const validateMail = () => {
-  //   const regex = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
-
-  //   return regex.test(form.mail)
-  // };
-
-
-  const handleSetForm = (event:any) => {
     const { name, value } = event.target;
+
     setForm({
       ...form,
-      [name]: value
+      [name]: value,
     });
+
     console.log(form.email);
-
-    if(form.email.length  !=0){
-      setErrorMail(true)
-      
-    } 
-    if(form.password.includes("")){
-      setErrorPassword(true)
-    
-    }
-  
-
   };
 
+  // Validate Mail
 
+  const validation = (email: string) => {
+    email = form.email;
+    const regex = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+    const val = regex.test(email);
+    return val;
+  };
+
+  const handleInputPassword = (event: any) => {
+    event.preventDefault();
+
+    if (event.target.value.length === 0) {
+      setErrorPassword(true);
+    }
+    if (event.target.value.length > 0) {
+      setErrorPassword(false);
+    }
+
+    const { name, value } = event.target;
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
+
+    console.log(form.password);
+  };
+
+  console.log(validation(form.email));
 
   return (
     <div className={styles.container}>
       <div className={styles.formcontainer}>
         <h2 className={styles.logintext}>Inicia sesión</h2>
-        <form action="" onSubmit={handleSubmit}>
+
+        <form action='' onSubmit={handleSubmit}>
           <div className={styles.inputbox}>
-            <label htmlFor="email" className={styles.label}>
+            <label htmlFor='email' className={styles.label}>
               Mail
             </label>
             <input
-              type="text"
-              id="email"
-              name="email"
+              type='text'
+              id='email'
+              name='email'
               value={form.email}
-              onChange={handleSetForm}
+              onChange={handleInputEmail}
               // onBlur={validateMail}
             />
             {errorMail && (
@@ -84,48 +110,47 @@ export const Login = () => {
                 <p>El campo mail es obligatorio</p>
               </div>
             )}
-            {/* {!errorMail && !isValid && (
+
+            {!errorMail && !isValid && (
               <div className={styles.errormail}>
                 <p>El mail no es válido</p>
-              </div> */}
-            {/* )} */}
+              </div>
+            )}
           </div>
           <div className={styles.inputbox}>
-            <label htmlFor="password" className={styles.label}>
+            <label htmlFor='password' className={styles.label}>
               Contraseña
             </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              value={form.password}
-              onChange={handleSetForm}
-            />
+            <input type='password' id='password' name='password' value={form.password} onChange={handleInputPassword} />
             {errorPassword && (
               <div className={styles.errorpass}>
                 <p>El campo Contraseña es obligatorio</p>
               </div>
             )}
-           
+            {message && (
+              <div className={styles.errorpass}>
+                <p>Hay campos inválidos</p>
+              </div>
+            )}
           </div>
 
           <div className={styles.forgotcontainer}>
-            <a href="#">olvidé mi contraseña </a>
+            <a href='#'>olvidé mi contraseña </a>
           </div>
 
           <div className={styles.btncontainer}>
-            <input type="submit" className={styles.btn} value="Login" />
+            <input type='submit' className={styles.btn} value='Login' />
           </div>
 
           <div className={styles.registerlink}>
             No tienes cuenta?
-            <a href="#">
+            <a href='#'>
               <span> Regístrate! </span>
             </a>
           </div>
 
           <div className={styles.backbtn}>
-            <a href="#">
+            <a href='#'>
               <FaArrowAltCircleLeft />
             </a>
           </div>
