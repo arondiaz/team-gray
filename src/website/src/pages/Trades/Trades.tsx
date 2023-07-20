683431a5dcca7f9bcc1d8976d64992c6f9b682e8
hernan-go <gobulinhernan@gmail.com>
2023-07-20 18:03:57 -0300

    (Trello ID: TxUdcNCC)(WIP) Add middle column for filtering UPs

--- src/website/src/pages/Trades/Trades.tsx
+++ src/website/src/pages/Trades/Trades.tsx
@@ -1,12 +1,11 @@
 import styles from './Trades.module.scss';
 import BgLayout from '../../shared/BgLayout';
+import TileApp from './CommonTile/Tile';
 
 export const Trades = () => {
 	return (
 		<div className={styles.homepage}>
-			<div className={styles['left-column']}>izquierda</div>
-			<div className={styles['middle-column']}>medio</div>
-			<div className={styles['right-column']}>derecha</div>
+			<TileApp />
 			<BgLayout />
 		</div>
 	);
