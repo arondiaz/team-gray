18f788a63dc0a6f7b35c905d3a67015e0f8d3115
NicoLoreto <nicoloreto.dev@gmail.com>
2023-07-24 21:44:11 -0300

    (Trello ID: Lo2wPV0y)(WIP) up class created

--- src/api/src/models/users/up.ts
+++ src/api/src/models/users/up.ts
@@ -1,1 +1,20 @@
-export class UP {}
+import { Gender, IUp } from "./up.interface";
+
+export class UP implements IUp {
+    id?: number = 0;
+    email: string = "";
+    name: string = "";
+    lastname: string = "";
+    dni: string = "";
+    province: string = "";
+    city: string = "";
+    tel: string = "";
+    link?: string = "";
+    about_me?: string = "";
+    gender: Gender.female;
+    birthdate: string = "";
+    auth_number?: string = "";
+    img?: string = "";
+    created_at?: string = "";
+    category_id: string = "";
+}
