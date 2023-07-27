a9ab35b88a10d8cf84b9425f2826031369939f02
NicoLoreto <nicoloreto.dev@gmail.com>
2023-07-27 18:11:40 -0300

    (Trello ID: f0t34569) endpoint get UPs by categories created

--- src/api/src/controllers/users.controller.ts
+++ src/api/src/controllers/users.controller.ts
@@ -25,4 +25,22 @@ export class UsersController extends ApiController {
             throw Error(error);
         }
     }
+
+    // get by category
+
+    @GET
+    @Path("/category/:category_id")
+    @Response<IUp[]>(200, "Succes")
+    @Response<string>(404, "There are no users with those trades")
+    @Response<string>(500, "No connection to database")
+    @Action({ route: "/category/:category_id" })
+    async getByCategory(@PathParam("category_id") category: number): Promise<IUp[]> {
+        try {
+            if (category <= 16) return await this.repo.find("category_id = ?", [category]);
+            this.httpContext.response.sendStatus(400).send("Bad request");
+        } catch (error) {
+            this.httpContext.response.sendStatus(500);
+            throw Error(error);
+        }
+    }
 }
