e35d76e4abae3ffddc6173488bfeaacdff318efb
aron0ne <arondiaz.cpn@gmail.com>
2023-07-29 16:08:35 -0300

    (Trello ID: kydKx0h5) implement error page

--- src/website/src/pages/404/ErrorPage.tsx
+++ src/website/src/pages/404/ErrorPage.tsx
@@ -1,3 +1,17 @@
+import styles from "./ErrorPage.module.scss";
+
 export const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <div className={styles.containererror}>
+      <h1 className={styles.number}>404</h1>
+      <h2 className={styles.notfound}>Página no encontrada</h2>
+      <p className={styles.backhome}>
+        Te pedimos disculpas, la página que solicitaste no existe.
+        <span>Por favor vuelve al inicio </span>
+      </p>
+      <button className={styles.homebtn}>
+        <a href="#">HOME</a>
+      </button>
+    </div>
+  );
 };
