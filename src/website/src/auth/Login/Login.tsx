fbdbb8b0b3a67a1765dbbc28f5b5b528b599b904
hernan-go <gobulinhernan@gmail.com>
2023-08-02 13:24:47 -0300

    (Trello ID: cLNiCPC6) Modify Login page styles and implement respective links

--- src/website/src/auth/Login/Login.tsx
+++ src/website/src/auth/Login/Login.tsx
@@ -1,99 +1,105 @@
-import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
-import { FaArrowAltCircleLeft } from "react-icons/fa";
-import styles from "./Login.module.scss";
-import BgLayout from "../../shared/BgLayout";
+import { useForm } from 'react-hook-form';
+import { FaArrowAltCircleLeft } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import styles from './Login.module.scss';
+import BgLayout from '../../shared/BgLayout';
+import BgOverlay from '../../shared/BgOverlay';
 
 export const Login = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    mode: "onChange",
-  });
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		mode: 'onChange',
+	});
 
-  const onSubmit = (data: any) => console.log(JSON.stringify(data));
+	const onSubmit = (data: any) => console.log(JSON.stringify(data));
 
-  return (
-    <>
-      {" "}
-      <div className={styles.container}>
-        <div className={styles.formcontainer}>
-          <h2 className={styles.logintext}>Inicia sesión</h2>
+	return (
+		<>
+			<div className={styles.loginContainer}>
+				<form
+					className={styles.formContainer}
+					action=''
+					onSubmit={handleSubmit(onSubmit)}>
+					<p className={styles.loginTitle}>Inicio sesión</p>
+					<div className={styles.mailContainer}>
+						<label htmlFor='email' className={styles.fieldsLabel}>
+							Mail
+						</label>
+						<input
+							autoComplete='off'
+							className={`${styles.fieldsInput} ${
+								errors.email ? styles.error : ''
+							}`}
+							type='text'
+							{...register('email', {
+								required: 'El mail es requerido',
+								pattern: {
+									value: /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/,
+									message: 'El mail no es válido',
+								},
+							})}
+						/>
+						{errors.email && (
+							<div className={styles.errorMessage}>
+								<span>{errors?.email?.message?.toString()}</span>
+							</div>
+						)}
+					</div>
+					<div className={styles.passwordContainer}>
+						<label htmlFor='password' className={styles.fieldsLabel}>
+							Contraseña
+						</label>
+						<input
+							type='password'
+							className={`${styles.fieldsInput} ${
+								errors.password ? styles.error : ''
+							}`}
+							{...register('password', {
+								required: 'La contraseña es requerida',
+								minLength: {
+									value: 8,
+									message: 'Longitud mínima 8 caracteres',
+								},
+								maxLength: {
+									value: 16,
+									message: 'Longitud máxima 16 caracteres',
+								},
+							})}
+						/>
+						{errors.password && (
+							<div className={styles.errorMessage}>
+								<span>{errors?.password?.message?.toString()}</span>
+							</div>
+						)}
+					</div>
+					<div className={styles.buttonLoginContainer}>
+						<button type='submit' className={styles.buttonLogin}>
+							login
+						</button>
+					</div>
+					<div>
+						<Link className={styles.signUpLink} to='/signup'>
+							Crear cuenta
+						</Link>
+					</div>
+					<div>
+						<Link to='/' className={styles.passwordForgotLink}>
+							Olvidé mi contraseña
+						</Link>
+					</div>
 
-          <form action="" onSubmit={handleSubmit(onSubmit)}>
-            <div className={styles.inputbox}>
-              <label htmlFor="email" className={styles.label}>
-                Mail
-              </label>
-              <input
-                autoComplete="off"
-                type="text"
-                {...register("email", {
-                  required: "El mail es obligatorio",
-                  pattern: {
-                    value: /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/,
-                    message: "El mail no es válido",
-                  },
-                })}
-              />
-              {errors.email && (
-                <div className={styles.errormail}>
-                  <span>{errors?.email?.message?.toString()}</span>
-                </div>
-              )}
-            </div>
-            <div className={styles.inputbox}>
-              <label htmlFor="password" className={styles.label}>
-                Contraseña
-              </label>
-              <input
-                type="password"
-                className={styles.input}
-                {...register("password", {
-                  required: "La contraseña es requerida",
-                  minLength: {
-                    value: 8,
-                    message: "Longitud mínima de 8 caracteres",
-                  },
-                  maxLength: {
-                    value: 16,
-                    message: "Longitud máxima de 16 caracteres",
-                  },
-                })}
-              />
-              {errors.password && (
-                <div className={styles.errorpass}>
-                  <span>{errors?.password?.message?.toString()}</span>
-                </div>
-              )}
-            </div>
-
-            <div className={styles.forgotcontainer}>
-              <a href="#">olvidé mi contraseña </a>
-            </div>
-
-            <div className={styles.btncontainer}>
-              <input type="submit" className={styles.btn} value="Login" />
-            </div>
-
-            <div className={styles.registerlink}>
-              No tienes cuenta?
-              <Link to="/signup">
-                <span>Regístrate</span>
-              </Link>
-            </div>
-
-            <div className={styles.backbtn}>
-              <Link to="/">
-                <FaArrowAltCircleLeft />
-              </Link>
-            </div>
-          </form>
-        </div>
-      </div>
-      <BgLayout />
-    </>
-  );
+					<div className={styles.backArrowContainer}>
+						<Link to='/' className={styles.backArrow}>
+							<FaArrowAltCircleLeft size={30} />
+						</Link>
+					</div>
+				</form>
+			</div>
+			<BgOverlay />
+			<BgLayout />
+		</>
+	);
 };
