089fc0411f19569dd56b55a0f38017519b56a451
NicoLoreto <nicoloreto.dev@gmail.com>
2023-08-15 23:54:48 -0300

    (Trello ID: yA3nY4cV) baseUrl and endpoints created

---
+++ src/website/src/services/endpoints.ts
@@ -0,0 +1,8 @@
+import { baseUrl } from './baseUrl';
+
+export const Endpoints = {
+  signupProfessionalUser: `${baseUrl}auth/signup`,
+  loginProfessionalUser: `${baseUrl}auth/login`,
+  categories: `${baseUrl}categories/`,
+  professionalUser: `${baseUrl}professional_user/`,
+};
