4bcf9e9e43fd393eb5f962a5c8ab5b8ad5669690
hernan-go <gobulinhernan@gmail.com>
2023-08-25 00:39:14 -0300

    (Trello ID: ROc7pOw8) Personal data of all professional users selected by category in column -A- are retrieved and displayed in column -B-

--- src/website/src/components/pages/Trades/B_CenterColumn/index.tsx
+++ src/website/src/components/pages/Trades/B_CenterColumn/index.tsx
@@ -1,95 +1,103 @@
 // CenterColumn.tsx
 import React, { useState, useEffect } from 'react';
-import TradeCard from '../Card';
-import styles from './CenterColumn.module.scss';
-import { UpData } from '../A_LeftColumn';
+import { TradeCard } from '../Card/index';
+import { IProfessionalUser } from '../../../../interfaces/ProfessionalUser.interface';
 import GenderFilter from '../GenderFilter';
-import SpinnerLogo from '../../../shared/Spinner'; // Importamos el componente LogoSpinner
+import SpinnerLogo from '../../../shared/Spinner';
+
+import styles from './CenterColumn.module.scss';
 
 interface CenterColumnProps {
-  tradeData: UpData[];
-  onCardClick: (person: UpData) => void;
+	tradeData: IProfessionalUser[];
+	onCardClick: (person: IProfessionalUser) => void;
 }
 
-const CenterColumn: React.FC<CenterColumnProps> = ({
-  tradeData,
-  onCardClick,
+export const CenterColumn: React.FC<CenterColumnProps> = ({
+	tradeData,
+	onCardClick,
 }) => {
-  const [isLoading, setIsLoading] = useState(false); // Estado para controlar el spinner
-  const [filteredData, setFilteredData] = useState<UpData[]>(tradeData);
-  const [genderFilter, setGenderFilter] = useState<string | null>(null);
+	const [isLoading, setIsLoading] = useState(false);
+	const [filteredData, setFilteredData] =
+		useState<IProfessionalUser[]>(tradeData);
+	const [genderFilter, setGenderFilter] = useState<string | null>(null);
 
-  useEffect(() => {
-    applyGenderFilter();
-  }, [genderFilter, tradeData]);
+	const calculateAge = (birthdate: string) => {
+		const today = new Date();
+		const birthDate = new Date(birthdate);
+		const ageInMilliseconds = today.getTime() - birthDate.getTime();
+		const ageInYears = Math.floor(
+			ageInMilliseconds / (365 * 24 * 60 * 60 * 1000)
+		);
+		return ageInYears;
+	};
 
-  const applyGenderFilter = () => {
-    if (genderFilter === null) {
-      // Si el género es null, mostrar todos los perfiles sin aplicar el filtro
-      setFilteredData(tradeData);
-    } else {
-      const filteredData = tradeData.filter(
-        (person) => person.gender.toLowerCase() === genderFilter.toLowerCase()
-      );
-      setFilteredData(filteredData);
-    }
-  };
+	useEffect(() => {
+		applyGenderFilter();
+	}, [genderFilter, tradeData]);
 
-  const handleGenderFilterChange = (selectedGender: string | null) => {
-    setGenderFilter(selectedGender);
-  };
+	const applyGenderFilter = () => {
+		if (genderFilter === null) {
+			setFilteredData(tradeData);
+		} else {
+			const filteredData = tradeData.filter(
+				(person) => person.gender.toLowerCase() === genderFilter.toLowerCase()
+			);
+			setFilteredData(filteredData);
+		}
+	};
 
-  const handleCardClick = (person: UpData) => {
-    setIsLoading(true); // Mostramos el spinner antes de realizar la carga de datos
+	const handleGenderFilterChange = (selectedGender: string | null) => {
+		setGenderFilter(selectedGender);
+	};
 
-    // Simulamos una carga de datos con un retraso de 2 segundos
-    setTimeout(() => {
-      setIsLoading(false); // Ocultamos el spinner después de la carga de datos
-      onCardClick(person); // Llamamos a la función onCardClick para mostrar la tarjeta en CenterColumn
-    }, 2000);
-  };
+	const handleCardClick = (person: IProfessionalUser) => {
+		setIsLoading(true);
 
-  return (
-    <>
-      <div className={styles.centerColumn}>
-        <div className={styles.filterContainer}>
-          <GenderFilter
-            genderFilter={genderFilter}
-            onGenderFilterChange={handleGenderFilterChange}
-          />
-        </div>
-        <div className={styles.scrollableList}>
-          {/* Se muestra SpinnerLogo si isLoading es true */}
-          {isLoading ? (
-            <div className={styles.spinnerContainer}>
-              <SpinnerLogo />
-            </div>
-          ) : (
-            // Mostramos los datos filtrados si isLoading es false
-            filteredData.map((trade) => (
-              <div key={trade.id} onClick={() => handleCardClick(trade)}>
-                <TradeCard
-                  name={trade.name}
-                  gender={trade.gender}
-                  species={trade.species}
-                  status={trade.status}
-                  image={trade.image}
-                  text={trade.text}
-                  styles={{
-                    tradeCard: styles.tradeCard,
-                    styleImageProfile: styles.styleImageProfile,
-                    tradeInfo: styles.tradeInfo,
-                    tradePhoto: styles.tradePhoto,
-                    tradeText: styles.tradeText,
-                  }}
-                />
-              </div>
-            ))
-          )}
-        </div>
-      </div>
-    </>
-  );
+		setTimeout(() => {
+			setIsLoading(false);
+			onCardClick({
+				...person,
+				age: calculateAge(person.birthdate),
+			});
+		}, 2000);
+	};
+
+	return (
+		<>
+			<div className={styles.centerColumn}>
+				<div className={styles.filterContainer}>
+					<GenderFilter
+						genderFilter={genderFilter}
+						onGenderFilterChange={handleGenderFilterChange}
+					/>
+				</div>
+				<div className={styles.scrollableList}>
+					{isLoading ? (
+						<div className={styles.spinnerContainer}>
+							<SpinnerLogo />
+						</div>
+					) : (
+						filteredData.map((m) => (
+							<div key={m.id} onClick={() => handleCardClick(m)}>
+								<TradeCard
+									img={m.img}
+									name={m.name}
+									lastName={m.lastname}
+									birthDate={m.birthdate}
+									gender={m.gender}
+									styles={{
+										tradeCard: styles.tradeCard,
+										styleImageProfile: styles.styleImageProfile,
+										tradeInfo: styles.tradeInfo,
+										tradePhoto: styles.tradePhoto,
+										tradeText: styles.tradeText,
+									}}
+								/>
+							</div>
+						))
+					)}
+				</div>
+			</div>
+		</>
+	);
 };
-
-export default CenterColumn;
