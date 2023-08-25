d86061b2b478391efa01b7dfbb3879ff39845997
hernan-go <gobulinhernan@gmail.com>
2023-08-25 00:13:28 -0300

    (Trello ID:PXcH1X8z) Add unique keys to routes for efficient rendering and updates

--- src/website/src/routes/RouteSwitch/RouteSwitch.tsx
+++ src/website/src/routes/RouteSwitch/RouteSwitch.tsx
@@ -4,16 +4,16 @@ import { routes } from '../routes';
 import { Footer } from '../../components/Footer';
 
 export const RouteSwitch = () => {
-  return (
-    <>
-      <Header />
-      <Routes>
-        {routes.map((route) => (
-          <Route path={route.url} element={route.element}></Route>
-        ))}
-      </Routes>
+	return (
+		<>
+			<Header />
+			<Routes>
+				{routes.map((route, index) => (
+					<Route key={index} path={route.url} element={route.element}></Route>
+				))}
+			</Routes>
 
-      <Footer />
-    </>
-  );
+			<Footer />
+		</>
+	);
 };
