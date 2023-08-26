6d92cbb21d06ec8468e7e7301fee86bff48ce5a5
NicoLoreto <nicoloreto.dev@gmail.com>
2023-08-26 19:44:00 -0300

    birth_date and last_name syntax updated

--- src/website/src/components/pages/Trades/B_CenterColumn/index.tsx
+++ src/website/src/components/pages/Trades/B_CenterColumn/index.tsx
@@ -8,96 +8,96 @@ import SpinnerLogo from '../../../shared/Spinner';
 import styles from './CenterColumn.module.scss';
 
 interface CenterColumnProps {
-	tradeData: IProfessionalUser[];
-	onCardClick: (person: IProfessionalUser) => void;
+  tradeData: IProfessionalUser[];
+  onCardClick: (person: IProfessionalUser) => void;
 }
 
 export const CenterColumn: React.FC<CenterColumnProps> = ({
-	tradeData,
-	onCardClick,
+  tradeData,
+  onCardClick,
 }) => {
-	const [isLoading, setIsLoading] = useState(false);
-	const [filteredData, setFilteredData] =
-		useState<IProfessionalUser[]>(tradeData);
-	const [genderFilter, setGenderFilter] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [filteredData, setFilteredData] =
+    useState<IProfessionalUser[]>(tradeData);
+  const [genderFilter, setGenderFilter] = useState<string | null>(null);
 
-	const calculateAge = (birthdate: string) => {
-		const today = new Date();
-		const birthDate = new Date(birthdate);
-		const ageInMilliseconds = today.getTime() - birthDate.getTime();
-		const ageInYears = Math.floor(
-			ageInMilliseconds / (365 * 24 * 60 * 60 * 1000)
-		);
-		return ageInYears;
-	};
+  const calculateAge = (birthdate: string) => {
+    const today = new Date();
+    const birthDate = new Date(birthdate);
+    const ageInMilliseconds = today.getTime() - birthDate.getTime();
+    const ageInYears = Math.floor(
+      ageInMilliseconds / (365 * 24 * 60 * 60 * 1000)
+    );
+    return ageInYears;
+  };
 
-	useEffect(() => {
-		applyGenderFilter();
-	}, [genderFilter, tradeData]);
+  useEffect(() => {
+    applyGenderFilter();
+  }, [genderFilter, tradeData]);
 
-	const applyGenderFilter = () => {
-		if (genderFilter === null) {
-			setFilteredData(tradeData);
-		} else {
-			const filteredData = tradeData.filter(
-				(person) => person.gender.toLowerCase() === genderFilter.toLowerCase()
-			);
-			setFilteredData(filteredData);
-		}
-	};
+  const applyGenderFilter = () => {
+    if (genderFilter === null) {
+      setFilteredData(tradeData);
+    } else {
+      const filteredData = tradeData.filter(
+        (person) => person.gender.toLowerCase() === genderFilter.toLowerCase()
+      );
+      setFilteredData(filteredData);
+    }
+  };
 
-	const handleGenderFilterChange = (selectedGender: string | null) => {
-		setGenderFilter(selectedGender);
-	};
+  const handleGenderFilterChange = (selectedGender: string | null) => {
+    setGenderFilter(selectedGender);
+  };
 
-	const handleCardClick = (person: IProfessionalUser) => {
-		setIsLoading(true);
+  const handleCardClick = (person: IProfessionalUser) => {
+    setIsLoading(true);
 
-		setTimeout(() => {
-			setIsLoading(false);
-			onCardClick({
-				...person,
-				age: calculateAge(person.birthdate),
-			});
-		}, 2000);
-	};
+    setTimeout(() => {
+      setIsLoading(false);
+      onCardClick({
+        ...person,
+        age: calculateAge(person.birth_date),
+      });
+    }, 2000);
+  };
 
-	return (
-		<>
-			<div className={styles.centerColumn}>
-				<div className={styles.filterContainer}>
-					<GenderFilter
-						genderFilter={genderFilter}
-						onGenderFilterChange={handleGenderFilterChange}
-					/>
-				</div>
-				<div className={styles.scrollableList}>
-					{isLoading ? (
-						<div className={styles.spinnerContainer}>
-							<SpinnerLogo />
-						</div>
-					) : (
-						filteredData.map((m) => (
-							<div key={m.id} onClick={() => handleCardClick(m)}>
-								<TradeCard
-									img={m.img}
-									name={m.name}
-									lastName={m.lastname}
-									birthDate={m.birthdate}
-									gender={m.gender}
-									styles={{
-										tradeCard: styles.tradeCard,
-										styleImageProfile: styles.styleImageProfile,
-										tradeInfo: styles.tradeInfo,
-										tradePhoto: styles.tradePhoto,
-										tradeText: styles.tradeText,
-									}}
-								/>
-							</div>
-						))
-					)}
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className={styles.centerColumn}>
+        <div className={styles.filterContainer}>
+          <GenderFilter
+            genderFilter={genderFilter}
+            onGenderFilterChange={handleGenderFilterChange}
+          />
+        </div>
+        <div className={styles.scrollableList}>
+          {isLoading ? (
+            <div className={styles.spinnerContainer}>
+              <SpinnerLogo />
+            </div>
+          ) : (
+            filteredData.map((m) => (
+              <div key={m.id} onClick={() => handleCardClick(m)}>
+                <TradeCard
+                  img={m.img}
+                  name={m.name}
+                  lastName={m.last_name}
+                  birthDate={m.birth_date}
+                  gender={m.gender}
+                  styles={{
+                    tradeCard: styles.tradeCard,
+                    styleImageProfile: styles.styleImageProfile,
+                    tradeInfo: styles.tradeInfo,
+                    tradePhoto: styles.tradePhoto,
+                    tradeText: styles.tradeText,
+                  }}
+                />
+              </div>
+            ))
+          )}
+        </div>
+      </div>
+    </>
+  );
 };
--- src/website/src/components/pages/Trades/C_RightColumn/index.tsx
+++ src/website/src/components/pages/Trades/C_RightColumn/index.tsx
@@ -7,112 +7,112 @@ import ProfileImages from '../assets/ProfileImage';
 import styles from './RightColumn.module.scss';
 
 interface RightColumnProps {
-	selectedPerson: IProfessionalUser | null;
+  selectedPerson: IProfessionalUser | null;
 }
 
 export const RightColumn: React.FC<RightColumnProps> = ({ selectedPerson }) => {
-	console.log({ selectedPerson });
+  console.log({ selectedPerson });
 
-	return (
-		<div className={styles.rightColumn}>
-			<div className={styles.topBanner}>
-				{' '}
-				Perfil Profesional:{' '}
-				{selectedPerson
-					? `${selectedPerson.name} ${selectedPerson.last_name}`
-					: 'Perfil Profesional'}
-			</div>
-			<div className={styles.coverImage}>
-				{selectedPerson && (
-					<ProfileImages categoryId={selectedPerson.category_id} />
-				)}
-			</div>
+  return (
+    <div className={styles.rightColumn}>
+      <div className={styles.topBanner}>
+        {' '}
+        Perfil Profesional:{' '}
+        {selectedPerson
+          ? `${selectedPerson.name} ${selectedPerson.last_name}`
+          : 'Perfil Profesional'}
+      </div>
+      <div className={styles.coverImage}>
+        {selectedPerson && (
+          <ProfileImages categoryId={selectedPerson.category_id} />
+        )}
+      </div>
 
-			{selectedPerson ? (
-				<div className={styles.profileInfo}>
-					<div className={styles.profileImage}></div>
-					{/* <img className={styles.profileImage} src={selectedPerson.img} /> */}
+      {selectedPerson ? (
+        <div className={styles.profileInfo}>
+          <div className={styles.profileImage}></div>
+          {/* <img className={styles.profileImage} src={selectedPerson.img} /> */}
 
-					<table className={styles.profileTable}>
-						<tbody>
-							<tr>
-								<th className={styles.profileTitle}>
-									Link:{' '}
-									<span className={styles.regularData}>
-										{selectedPerson.link}
-									</span>
-								</th>
-							</tr>
-							<tr>
-								<th className={styles.profileTitle}>
-									Nombre:{' '}
-									<span className={styles.regularData}>
-										{selectedPerson.name} {selectedPerson.last_name}
-									</span>
-								</th>
-							</tr>
+          <table className={styles.profileTable}>
+            <tbody>
+              <tr>
+                <th className={styles.profileTitle}>
+                  Link:{' '}
+                  <span className={styles.regularData}>
+                    {selectedPerson.link}
+                  </span>
+                </th>
+              </tr>
+              <tr>
+                <th className={styles.profileTitle}>
+                  Nombre:{' '}
+                  <span className={styles.regularData}>
+                    {selectedPerson.name} {selectedPerson.last_name}
+                  </span>
+                </th>
+              </tr>
 
-							<tr>
-								<th className={styles.profileTitle}>
-									Edad:{' '}
-									<span className={styles.regularData}>
-										{calculateAge(selectedPerson.birthdate)}
-									</span>
-								</th>
-							</tr>
+              <tr>
+                <th className={styles.profileTitle}>
+                  Edad:{' '}
+                  <span className={styles.regularData}>
+                    {calculateAge(selectedPerson.birth_date)}
+                  </span>
+                </th>
+              </tr>
 
-							<tr>
-								<th className={styles.profileTitle}>
-									Email:{' '}
-									<span className={styles.regularData}>
-										{selectedPerson.email}
-									</span>
-								</th>
-							</tr>
+              <tr>
+                <th className={styles.profileTitle}>
+                  Email:{' '}
+                  <span className={styles.regularData}>
+                    {selectedPerson.email}
+                  </span>
+                </th>
+              </tr>
 
-							<tr>
-								<th className={styles.profileTitle}>
-									Ciudad:{' '}
-									<span className={styles.regularData}>
-										{selectedPerson.city}
-									</span>
-								</th>
-							</tr>
-							<tr>
-								<th className={styles.profileTitle}>
-									Teléfono:{' '}
-									<span className={styles.regularData}>
-										{selectedPerson.tel}
-									</span>
-								</th>
-							</tr>
+              <tr>
+                <th className={styles.profileTitle}>
+                  Ciudad:{' '}
+                  <span className={styles.regularData}>
+                    {selectedPerson.city}
+                  </span>
+                </th>
+              </tr>
+              <tr>
+                <th className={styles.profileTitle}>
+                  Teléfono:{' '}
+                  <span className={styles.regularData}>
+                    {selectedPerson.tel}
+                  </span>
+                </th>
+              </tr>
 
-							<tr>
-								<th className={styles.profileTitle}>
-									Género:{' '}
-									<span className={styles.regularData}>
-										{selectedPerson.gender}
-									</span>
-								</th>
-							</tr>
+              <tr>
+                <th className={styles.profileTitle}>
+                  Género:{' '}
+                  <span className={styles.regularData}>
+                    {selectedPerson.gender}
+                  </span>
+                </th>
+              </tr>
 
-							<tr>
-								<th className={styles.profileTitle}>
-									Número de Autorización:{' '}
-									<span className={styles.regularData}>
-										{selectedPerson.auth_number}
-									</span>
-								</th>
-							</tr>
-						</tbody>
-					</table>
-					<div className={styles.scrollableText}>
-						<p>Sobre mí: {selectedPerson.about_me}</p>
-					</div>
-				</div>
-			) : (
-				<p>Selecciona un profesional para ver su perfil completo</p>
-			)}
-		</div>
-	);
+              <tr>
+                <th className={styles.profileTitle}>
+                  Número de Autorización:{' '}
+                  <span className={styles.regularData}>
+                    {selectedPerson.auth_number}
+                  </span>
+                </th>
+              </tr>
+            </tbody>
+          </table>
+          <div className={styles.scrollableText}>
+            <p>Sobre mí: {selectedPerson.about_me}</p>
+          </div>
+        </div>
+      ) : (
+        <p>Selecciona un profesional para ver su perfil completo</p>
+      )}
+    </div>
+  );
 };
