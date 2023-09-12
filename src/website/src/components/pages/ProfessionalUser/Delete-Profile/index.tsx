3c4d4690deceecf260631367554ad32dd51b6655
Aron Diaz <arondiaz.cpn@gmail.com>
2023-09-12 16:37:23 -0300

    (Trello ID: RnvsEhwQ) Updated delite profile alerts

--- src/website/src/components/pages/ProfessionalUser/Delete-Profile/index.tsx
+++ src/website/src/components/pages/ProfessionalUser/Delete-Profile/index.tsx
@@ -12,35 +12,39 @@ export const DeleteProfile: React.FC<DeleteProfileProps> = ({
 }) => {
   const { request } = useDelete();
 
+  const notification = (color: string) => ({
+    position: toast.POSITION.BOTTOM_RIGHT,
+    autoClose: 5000,
+    style: {
+      fontWeight: 'bold',
+      border: `0.1rem solid ${color}`,
+    },
+  });
+
   const handleDelete = async () => {
     const response = await request();
 
     // TODO: Handle responses to display modals based on the status.
     switch (response?.status) {
       case 204:
-        toast.success('Cuenta eliminada!', {
-          position: toast.POSITION.BOTTOM_RIGHT,
-          autoClose: 5000,
-          style: {
-            fontWeight: 'bold',
-            border: '0.1rem solid green',
-          },
-        });
+        toast.success(
+          '¡Cuenta eliminada correctamente!',
+          notification('green')
+        );
         localStorage.clear();
         break;
       case 500:
-        toast.error('Error, por favor inténtelo de nuevo.', {
-          position: toast.POSITION.BOTTOM_RIGHT,
-          autoClose: 5000,
-          style: {
-            fontWeight: 'bold',
-            border: '0.1rem solid red',
-          },
-        });
+        toast.error(
+          'Error al eliminar el perfil, por favor inténtelo de nuevo.',
+          notification('red')
+        );
         break;
       default:
-        console.log('default');
-        toast.info('Default');
+        toast.error(
+          'Error, por favor inténtelo de nuevo.',
+          notification('red')
+        );
+        break;
     }
   };
 
