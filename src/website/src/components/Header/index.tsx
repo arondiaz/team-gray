4310c17d7c7d46e18ac5bc9ef4e439ee241256d2
hernan-go <gobulinhernan@gmail.com>
2023-09-13 18:45:04 -0300

    (Trello ID: pgfqd5xb) add logout button to navbar

--- src/website/src/components/Header/index.tsx
+++ src/website/src/components/Header/index.tsx
@@ -2,26 +2,21 @@ import { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { BiMenuAltRight } from 'react-icons/bi';
 import { AiOutlineClose } from 'react-icons/ai';
+import { FaSignOutAlt } from 'react-icons/fa';
 import { routes } from '../../routes/routes';
 import classes from './Header.module.scss';
 
-{
-	/* State to control the menu open/close */
-}
+
 const Header = () => {
 	const [menuOpen, setMenuOpen] = useState(false);
 
-	{
-		/* State to store window size*/
-	}
+	
 	const [size, setSize] = useState({
 		width: 0,
 		height: 0,
 	});
 
-	{
-		/* Handle window resize event */
-	}
+	
 	useEffect(() => {
 		const handleResize = () => {
 			setSize({
@@ -32,26 +27,24 @@ const Header = () => {
 
 		window.addEventListener('resize', handleResize);
 
-		{
-			/* Cleanup event listener on component unmount */
-		}
+		
 		return () => window.removeEventListener('resize', handleResize);
 	}, []);
 
-	{
-		/* Close menu if window width is greater than 768px */
-	}
+	
 	useEffect(() => {
 		if (size.width > 768) {
 			setMenuOpen(false);
 		}
 	}, [size.width]);
 
-	{
-		/* Toggle menu open/close state */
-	}
+	
 	const menuToggleHandler = () => {
 		setMenuOpen((prevMenuOpen) => !prevMenuOpen);
+	};
+
+	const handleLogout = () => {
+		// Logica del Logout(consultar)
 	};
 
 	return (
@@ -85,10 +78,14 @@ const Header = () => {
 								)}
 							</li>
 						))}
+						<li>
+							<button className={classes.logoutButton} onClick={handleLogout}>
+								<FaSignOutAlt size={24} />{' '}
+							</button>
+						</li>
 					</ul>
 				</nav>
 				<div className={classes.header__content__toggle}>
-					{/* Menu toggle button */}
 
 					{!menuOpen ? (
 						<BiMenuAltRight onClick={menuToggleHandler} size={40} />
