24a922ef8a8ab86483f8ff6a8b78a5e9da648f76
NicoLoreto <nicoloreto.dev@gmail.com>
2023-09-13 20:04:36 -0300

    (Trello ID: 4FVOcy6w) try and catch in edit service added

--- src/api/src/service/ProfessionalUser.service.ts
+++ src/api/src/service/ProfessionalUser.service.ts
@@ -74,152 +74,155 @@ export class ProfessionalUserService {
     }
 
     public async edit(professionalUser: IProfessionalUser): Promise<IResponse> {
-        // Create a user for update.
-        const updatedUser: IProfessionalUser = {
-            // Assign the id to the authenticated user.
-            id: this.service.authUser.id,
-            // Assign the email to the authenticated user.
-            email: this.service.authUser.email,
-            name: professionalUser.name,
-            last_name: professionalUser.last_name,
-            dni: professionalUser.dni,
-            birth_date: professionalUser.birth_date,
-            category_id: professionalUser.category_id,
-            link: professionalUser.link,
-            tel: professionalUser.tel,
-            city: professionalUser.city,
-            province: professionalUser.province,
-            gender: professionalUser.gender,
-            auth_number: professionalUser.auth_number,
-            about_me: professionalUser.about_me,
-        };
+        try {
+            // Create a user for update.
+            const updatedUser: IProfessionalUser = {
+                // Assign the id to the authenticated user.
+                id: this.service.authUser.id,
+                // Assign the email to the authenticated user.
+                email: this.service.authUser.email,
+                name: professionalUser.name,
+                last_name: professionalUser.last_name,
+                dni: professionalUser.dni,
+                birth_date: professionalUser.birth_date,
+                category_id: professionalUser.category_id,
+                link: professionalUser.link,
+                tel: professionalUser.tel,
+                city: professionalUser.city,
+                province: professionalUser.province,
+                gender: professionalUser.gender,
+                auth_number: professionalUser.auth_number,
+                about_me: professionalUser.about_me,
+            };
 
-        // If the value is undefined assign the value of the auth user.
-        for (let key in updatedUser) {
-            if (!updatedUser[key]) {
-                updatedUser[key] = this.service.authUser[key];
+            // If the value is undefined assign the value of the auth user.
+            for (let key in updatedUser) {
+                if (!updatedUser[key]) {
+                    updatedUser[key] = this.service.authUser[key];
+                }
             }
+
+            const editArr = [
+                updatedUser.name,
+                updatedUser.last_name,
+                updatedUser.dni,
+                updatedUser.birth_date,
+                updatedUser.category_id,
+                updatedUser.link,
+                updatedUser.tel,
+                updatedUser.city,
+                updatedUser.province,
+                updatedUser.gender,
+                updatedUser.auth_number,
+                updatedUser.about_me,
+            ];
+
+            if (this.validate.validateStringTypeData(editArr)) {
+                return {
+                    error: true,
+                    message: "There are fields that are not of type string",
+                    code: 400,
+                };
+            }
+
+            if (!this.validate.validateString(updatedUser.name)) {
+                return {
+                    error: true,
+                    message: "The field is not a valid name",
+                    code: 400,
+                };
+            }
+
+            if (!this.validate.validatePhone(updatedUser.tel)) {
+                return {
+                    error: true,
+                    message: "The field is not a valid phone",
+                    code: 400,
+                };
+            }
+
+            if (!this.validate.validateString(updatedUser.last_name)) {
+                return {
+                    error: true,
+                    message: "The field is not a valid last name",
+                    code: 400,
+                };
+            }
+
+            if (!this.validate.validateString(updatedUser.city)) {
+                return {
+                    error: true,
+                    message: "The field is not a valid city",
+                    code: 400,
+                };
+            }
+
+            if (!this.validate.validateDni(updatedUser.dni)) {
+                return {
+                    error: true,
+                    message: "The field is not a valid DNI",
+                    code: 400,
+                };
+            }
+
+            if (!this.validate.validateString(updatedUser.province)) {
+                return {
+                    error: true,
+                    message: "The field is not a valid province",
+                    code: 400,
+                };
+            }
+
+            if (this.validate.validateAge(updatedUser.birth_date)) {
+                return {
+                    error: true,
+                    message: this.validate.validateAge(updatedUser.birth_date) as string,
+                    code: 400,
+                };
+            }
+
+            if (this.validate.validateGender(updatedUser.gender)) {
+                return {
+                    error: true,
+                    message: "Gender must be 'Male', 'Female' or 'Non-binary'",
+                    code: 400,
+                };
+            }
+
+            const user: IProfessionalUser = await this.repo.update(updatedUser);
+
+            if (user) {
+                const config = this.configBuilder.build(Configuration);
+
+                const token = jwt.sign(
+                    {
+                        email: user.email,
+                        name: user.name,
+                        last_name: user.last_name,
+                        dni: user.dni,
+                        province: user.province,
+                        city: user.city,
+                        tel: user.tel,
+                        link: user.link,
+                        about_me: user.about_me,
+                        gender: user.gender,
+                        birth_date: user.birth_date,
+                        auth_number: user.auth_number,
+                        img: user.img,
+                        category_id: user.category_id,
+                    },
+                    config.jwt.secret
+                );
+                return {
+                    error: false,
+                    message: "Updated user",
+                    code: 200,
+                    token: token,
+                };
+            }
+        } catch (error) {
+            throw new Error(error);
         }
-
-        const editArr = [
-            updatedUser.name,
-            updatedUser.last_name,
-            updatedUser.dni,
-            updatedUser.birth_date,
-            updatedUser.category_id,
-            updatedUser.link,
-            updatedUser.tel,
-            updatedUser.city,
-            updatedUser.province,
-            updatedUser.gender,
-            updatedUser.auth_number,
-            updatedUser.about_me,
-        ];
-
-        if (this.validate.validateStringTypeData(editArr)) {
-            return {
-                error: true,
-                message: "There are fields that are not of type string",
-                code: 400,
-            };
-        }
-
-        if (!this.validate.validateString(updatedUser.name)) {
-            return {
-                error: true,
-                message: "The field is not a valid name",
-                code: 400,
-            };
-        }
-
-        if (!this.validate.validatePhone(updatedUser.tel)) {
-            return {
-                error: true,
-                message: "The field is not a valid phone",
-                code: 400,
-            };
-        }
-
-        if (!this.validate.validateString(updatedUser.last_name)) {
-            return {
-                error: true,
-                message: "The field is not a valid last name",
-                code: 400,
-            };
-        }
-
-        if (!this.validate.validateString(updatedUser.city)) {
-            return {
-                error: true,
-                message: "The field is not a valid city",
-                code: 400,
-            };
-        }
-
-        if (!this.validate.validateDni(updatedUser.dni)) {
-            return {
-                error: true,
-                message: "The field is not a valid DNI",
-                code: 400,
-            };
-        }
-
-        if (!this.validate.validateString(updatedUser.province)) {
-            return {
-                error: true,
-                message: "The field is not a valid province",
-                code: 400,
-            };
-        }
-
-        if (this.validate.validateAge(updatedUser.birth_date)) {
-            return {
-                error: true,
-                message: this.validate.validateAge(updatedUser.birth_date) as string,
-                code: 400,
-            };
-        }
-
-        if (this.validate.validateGender(updatedUser.gender)) {
-            return {
-                error: true,
-                message: "Gender must be 'Male', 'Female' or 'Non-binary'",
-                code: 400,
-            };
-        }
-
-        const user: IProfessionalUser = await this.repo.update(updatedUser);
-
-        if (user) {
-            const config = this.configBuilder.build(Configuration);
-
-            const token = jwt.sign(
-                {
-                    email: user.email,
-                    name: user.name,
-                    last_name: user.last_name,
-                    dni: user.dni,
-                    province: user.province,
-                    city: user.city,
-                    tel: user.tel,
-                    link: user.link,
-                    about_me: user.about_me,
-                    gender: user.gender,
-                    birth_date: user.birth_date,
-                    auth_number: user.auth_number,
-                    img: user.img,
-                    category_id: user.category_id,
-                },
-                config.jwt.secret
-            );
-            return {
-                error: false,
-                message: "Updated user",
-                code: 200,
-                token: token,
-            };
-        }
-        throw new Error("User could not edit");
     }
 
     public async changeStateToFalse(authUser: IProfessionalUser): Promise<IResponse> {
